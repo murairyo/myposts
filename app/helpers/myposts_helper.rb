741424a5f874813287b1d4c67f734e1ebb996452
murai1027 <rzdb3693@gmail.com>
2018-03-28 06:08:34 +0000

    修正

--- app/helpers/myposts_helper.rb
+++ app/helpers/myposts_helper.rb
@@ -1,9 +1,9 @@
 module MypostsHelper
-def choose_new_or_edit
+  def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
       confirm_myposts_path
     elsif action_name == 'edit'
-      mypost_path
+      myposts_path
     end
   end
 end
